23ee40ad30ab500288332efbe507708aebe5740b
Jhenna Foronda <jfcaldetera@gmail.com>
2018-06-26 17:36:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 function theBeatlesPlay(musicians, instruments)
 {
-  var master - new Array();
+  var master = new Array();
 
   for (var i = 0; i <= musicians.length; i++)
   {
-    master,push("${musicians[i]} plays ${instruments[i]}")
+    master.push("${musicians[i]} plays ${instruments[i]}")
+  }
+  return master
+}
+
+function johnLennonFacts(facts)
+{
+  var count = facts.length
+  var master = new Array()
+  
+  while (count != 0)
+  {
+    master.push(facts[i] + "!!!")
   }
   return master
 }
