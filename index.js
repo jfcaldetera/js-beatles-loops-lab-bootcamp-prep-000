7bdf19713ccbe9306556f7590f7706020c983664
Jhenna Foronda <jfcaldetera@gmail.com>
2018-06-26 17:30:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay(musicians, instruments)
+{
+  var master - new Array();
+
+  for (var i = 0; i <= musicians.length; i++)
+  {
+    master,push("${musicians[i]} plays ${instruments[i]}")
+  }
+  return master
+}
